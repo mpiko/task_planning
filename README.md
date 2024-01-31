cd56beb36cb42c99d37ec24475515b7744d57e3a
Michael Piko <michael@piko.com.au>
2024-02-01 09:44:02 +1100

    inital add

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # task_planning
+
+Puts the date fields back on the task form.
+
+Use at own risk.
+
+
---
+++ field_form_fix/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Field Form Fix',
+    'version': '1.2',
+    'summary': 'puts planning dates back on the form',
+    'category': '',
+    'description': """
+Puts the planning dates back on the field services form
+""",
+    'website': 'https://www.cyder.com.au',
+    'depends': ['industry_fsm'],
+    'data': [
+        'views/field.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
